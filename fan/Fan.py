e1c244efd12abae462d3c32eea54341d89f561ae
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-02 12:04:15 +0800

    Provide requirements for fan class

--- fan/Fan.py
+++ fan/Fan.py
@@ -1,4 +1,41 @@
 class Fan:
+    '''
+    A class to represent an electric fan
+
+    ...
+
+    Attributes
+    ----------
+    __speed : str
+        the electric fan's speed
+    __power : bool
+        specifies whether fan is on or off
+    __radius : float
+        the electric fan's radius
+    __color: str
+        the electric fan's color
+    
+    Methods
+    -------
+    get_speed
+        gets the electric fan's speed
+    set_speed
+        sets the electric fan's speed
+    get_power
+        gets the electric fan's power state
+    set_power
+        sets the electric fan's power state
+    get_radius
+        gets the electric fan's radius
+    set_radius
+        sets the electric fan's radius
+    get_color
+        gets the electric fan's color
+    set_color
+        sets the electric fan's color
+    
+    '''
+
     SLOW = 1
     MEDIUM = 2
     FAST = 3
