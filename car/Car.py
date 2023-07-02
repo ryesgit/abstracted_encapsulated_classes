bf8de5cdbf072bf79bd93194943c76f9b6174b81
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-02 19:55:33 +0800

    Define requirements for Car class

---
+++ car/Car.py
@@ -0,0 +1,25 @@
+class Car:
+    '''
+    A class to instantiate car objects
+
+    ...
+
+    Attributes
+    ----------
+    __year_model : str
+        the year when the car was made
+    __maker : str
+        the car's manufacturer information
+    __speed : str
+        the car's speed
+    
+    Methods
+    -------
+    get_speed
+        gets the car's speed
+    accelerate
+        adds 5 to the car's speed
+    brake
+        deducts 5 from the car's speed
+    
+    '''
