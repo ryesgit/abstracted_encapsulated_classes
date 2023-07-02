047ca52c53f0ba132196a43e8bf72aa17866da24
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-02 22:29:08 +0800

    Display pet details

--- pet/PetPlayground.py
+++ pet/PetPlayground.py
@@ -5,3 +5,13 @@ from Pet import Pet
 name = input("Input pet name: ")
 animal_type = input("Input pet animal type: ")
 age = int(input("Input pet age: "))
+
+def is_vowel(letter:chr):
+    return True if letter.lower() in ['a', 'e', 'i', 'o', 'u'] else False
+
+def display_pet_details(pet:Pet):
+    print(f"Hello! My name is {pet.get_name()}, I am {'an' if is_vowel(pet.get_animal_type()[0]) else 'a'} {pet.get_animal_type()}.")
+    print(f"I am {pet.get_age()} years old!")
+
+pet = Pet(name, animal_type, age)
+display_pet_details(pet)
