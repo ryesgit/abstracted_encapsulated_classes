4eef41c7afcfc732164bb4c7bb35a9fe13d177d1
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-02 21:44:57 +0800

    Define requirements for Pet class

---
+++ pet/Pet.py
@@ -0,0 +1,30 @@
+class Pet:
+    '''
+        A class representing a pet
+
+        Attributes
+        ----------
+
+        __name : str
+            Name of the pet
+        __animal_type : str
+            Animal type of pet (could be 'Dog', 'Cat', 'Lion', etc)
+        __age : int
+            The pet's age
+
+        Methods
+        -------
+
+        get_name
+            returns a string (name of the pet)
+        set_name
+            sets the pet's name; receives str
+        get_animal_type
+            returns the pet's animal type
+        set_animal_type
+            sets the pet's animal type; receives str
+        get_age
+            returns the pet's animal type
+        set_age
+            sets pet's age; returns int
+    '''
