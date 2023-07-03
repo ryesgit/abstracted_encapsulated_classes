cb57c008d6456ea6d77f9e0fcc83c1ca9db0521c
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-03 12:40:31 +0800

    Build two tk instances; on and off fan state
    Apparently, tkinter.tk can only be used once.
    
    Before, there are conflicts when instantiating two tk widget instances.
    
    With the updates, tkinter now builds independent instances.

--- fan/FanVisualTest.py
+++ fan/FanVisualTest.py
@@ -2,9 +2,14 @@ from FanApp import FanApp
 from Fan import Fan
 import tkinter
 
-root = tkinter.Tk()
+root = tkinter.Toplevel()
+root2 = tkinter.Toplevel()
 
 fan = Fan(True, 3)
 fan_app = FanApp(fan, root, image_path='./fan/assets/fan.png')
 
+closed_fan = Fan(False, 1)
+closed_fan_app = FanApp(closed_fan, root2, image_path='./fan/assets/fan.png')
+
+root2.mainloop()
 root.mainloop()
