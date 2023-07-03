d7468fd1b2e939ac739263206df448d88fc5e28a
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-03 12:42:50 +0800

    Use previously created fans from FanTest

--- fan/FanVisualTest.py
+++ fan/FanVisualTest.py
@@ -2,14 +2,13 @@ from FanApp import FanApp
 from Fan import Fan
 import tkinter
 
+from FanTest import first_fan, second_fan
+
 root = tkinter.Toplevel()
 root2 = tkinter.Toplevel()
 
-fan = Fan(True, 3)
-fan_app = FanApp(fan, root, image_path='./fan/assets/fan.png')
-
-closed_fan = Fan(False, 1)
-closed_fan_app = FanApp(closed_fan, root2, image_path='./fan/assets/fan.png')
+first_fan_app = FanApp(first_fan, root, image_path='./fan/assets/fan.png')
+second_fan_app = FanApp(second_fan, root2, image_path='./fan/assets/fan.png')
 
 root2.mainloop()
 root.mainloop()
