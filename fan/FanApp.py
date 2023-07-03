1f6306cf38814899b17e25e47e832950e2fb6b3d
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-03 10:17:57 +0800

    Define requirements for FanApp class

--- fan/FanApp.py
+++ fan/FanApp.py
@@ -1,2 +1,22 @@
 from PIL import Image, ImageTk
 import tkinter as tk
+from Fan import Fan
+
+class FanApp:
+    '''
+    Attributes
+    ----------
+    fan : Fan
+        The fan instance from the Fan Class
+    master : Tk
+        The tk widget; main window of the app
+    canvas : Tk.Canvas
+        The application's canvas (dimensions are 500 x 500)
+
+    Methods
+    -------
+    draw_image
+        draw image onto canvas
+    rotate_image
+        rotate image inside canvas
+    '''
