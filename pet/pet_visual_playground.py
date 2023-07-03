86d6e3f1c99ec4c83e2eab8ccc1ab3e4d4b9af84
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-03 18:37:51 +0800

    Add attribution for cat image

--- pet/pet_visual_playground.py
+++ pet/pet_visual_playground.py
@@ -5,6 +5,9 @@ pet = Pet('Rudolf', 'Cat', 4)
 
 root = tk.Tk()
 
+# Credits to Flat Icons for the cat icon
+# https://www.flaticon.com/authors/flat-icons
+
 pet_app = PetApp(root, pet, './pet/assets/cat.png')
 
 root.mainloop()
