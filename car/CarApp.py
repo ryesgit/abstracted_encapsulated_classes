65f596c8e77b7487f342007b9107622c9b9d2204
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-07-03 15:07:16 +0800

    Instantiate Car app

--- car/CarApp.py
+++ car/CarApp.py
@@ -21,3 +21,21 @@ class CarApp:
         moves car along plane
         according to speed
     '''
+    def __init__(self, car: Car, master: tk.Tk, image_path: str) -> None:
+        '''
+        Parameters
+        ----------
+        car : Car
+            the car instance
+        master : tk.Tk
+            the Tkinter widget instance
+        image_path : str
+            the path of the car image
+        '''
+
+        self.__car = car
+        self.__master = master
+        image = Image.open(image_path)
+        self.__image = image
+        self.__canvas = tk.Canvas(master, width=(self.__image.size[0] * 5), height=(self.__image.size[1] + 20))
+        self.__canvas.pack()
---
+++ car/CarVisualTest.py
@@ -0,0 +1,14 @@
+from CarApp import CarApp
+from Car import Car
+import tkinter as tk
+
+car = Car(2003, 'Honda', 20)
+root = tk.Tk()
+
+
+# Credits to Vector Stall for the race car icon!
+# https://www.flaticon.com/authors/vector-stall
+
+car_app = CarApp(car, root, './car/assets/race-car.png')
+
+root.mainloop()
